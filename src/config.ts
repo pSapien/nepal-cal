64c5fa91c66d873a5762850c2c32ad3210e0e34d
psapien <prateekthapa26@gmail.com>
2026-06-22 18:46:38 +0545

    refactor: move month names/aliases to config

---
+++ src/config.ts
@@ -0,0 +1,29 @@
+export const MONTH_NAMES = [
+  "Baisakh",
+  "Jestha",
+  "Ashadh",
+  "Shrawan",
+  "Bhadra",
+  "Ashwin",
+  "Kartik",
+  "Mangsir",
+  "Poush",
+  "Magh",
+  "Falgun",
+  "Chaitra",
+];
+
+export const MONTH_ALIASES: Record<string, number> = {
+  baisakh: 0, baishakh: 0, bai: 0,
+  jestha: 1, jeth: 1, je: 1, jet: 1,
+  ashadh: 2, asar: 2, ashad: 2, asad: 2, ash: 2, as: 2,
+  shrawan: 3, saun: 3, shravan: 3, shr: 3,
+  bhadra: 4, bhadau: 4, bhad: 4,
+  ashwin: 5, ashoj: 5, asoj: 5,
+  kartik: 6, karthik: 6, kar: 6,
+  mangsir: 7, mansir: 7, manghsir: 7, 
+  poush: 8, pus: 8, poosh: 8, push: 8,
+  magh: 9, mag: 9,
+  falgun: 10, fagun: 10, phalgun: 10, fal: 10,
+  chaitra: 11, chait: 11, chai: 11,
+};
